ba9d78f27e9fd80e823adb4e0b8a9fd8d6de0a9a
dspwithaheart <aayush1312@live.com>
2020-05-14 00:48:17 +0200

    added dist

---
+++ precache-manifest.a1c356eb8464adc2f0666eed31ab242a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61aaa8edfceb3866c0bb",
+    "url": "/vue_pwa/css/about.9e450cc9.css"
+  },
+  {
+    "revision": "e699df00d16aaf6dc202",
+    "url": "/vue_pwa/css/chunk-vendors.dae7d16f.css"
+  },
+  {
+    "revision": "21c15818ec200eb38a2dedc7c1571a86",
+    "url": "/vue_pwa/img/map/bump.jpg"
+  },
+  {
+    "revision": "948042670dc00af3b9ed306f9ac9dfef",
+    "url": "/vue_pwa/img/map/map.jpg"
+  },
+  {
+    "revision": "f384f88fb53cbc3bf236ffff8c905d93",
+    "url": "/vue_pwa/img/map/night.jpg"
+  },
+  {
+    "revision": "1d8422982793712f994e0b504349c4b3",
+    "url": "/vue_pwa/img/map/spekular.jpg"
+  },
+  {
+    "revision": "e21d2ba042e4744bbb06e814c6c33955",
+    "url": "/vue_pwa/index.html"
+  },
+  {
+    "revision": "61aaa8edfceb3866c0bb",
+    "url": "/vue_pwa/js/about.f30b0483.js"
+  },
+  {
+    "revision": "89d04a0726fe09de43e5",
+    "url": "/vue_pwa/js/app.bc74bedc.js"
+  },
+  {
+    "revision": "e699df00d16aaf6dc202",
+    "url": "/vue_pwa/js/chunk-vendors.f8d4a084.js"
+  },
+  {
+    "revision": "f507a4068778bddc57cf498ec83cacf2",
+    "url": "/vue_pwa/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue_pwa/robots.txt"
+  }
+]);
